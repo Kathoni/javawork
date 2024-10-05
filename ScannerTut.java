24d641dc97111ed5a66738032ce3bc7924631115
Kathoni <thrusherkeys@gmail.com>
2024-10-05 19:00:12 +0300

    navigating java.util library while accessing scanner

---
+++ ScannerTut.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class ScannerTut {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);//Creating a new scanner object
+        System.out.println("Enter Name:");//This asks the user to input
+        String name = scan.nextLine();//This is a scanner method used in casting the data correctly 
+
+        System.out.println("Enter Age:");//This asks the user to input
+        int age = scan.nextInt();//This is a scanner method used in casting the data correctly
+
+        System.out.println("Greetings " + name
+                + ". You are " + age); //used to create output for the program
+
+    }
+}
