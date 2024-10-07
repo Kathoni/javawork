c504c9dd12a5cc76f83f01c0bdf7252f5282a082
Kathoni <thrusherkeys@gmail.com>
2024-10-07 18:24:36 +0300

    adding @overide after tostring ()

--- Person.java
+++ Person.java
@@ -31,7 +31,7 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
-
+    @Override
     public String toString() {
         return name + " \n" 
         + secondname + "\n "
