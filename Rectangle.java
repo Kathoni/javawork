b1210ce241a1444a57b3ceeaa24859ff708b119b
Kathoni <thrusherkeys@gmail.com>
2024-10-07 14:59:55 +0300

    class

---
+++ Rectangle.java
@@ -0,0 +1,46 @@
+
+
+public class Rectangle{
+    double length;
+    double width;
+
+    public Rectangle() {
+    }
+    
+    public Rectangle(double length, double width) {
+        this.length = length;//means the upper length
+        this.width = width;
+    }
+    
+    public double getLength() {
+        return length;
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+
+    public void setLength(double newlength) {
+        this.length = newlength;
+    }
+        public void setWidth(double newwidth) {
+          this.width = newwidth;
+             }
+      public double findArea(){
+        return length*width;
+      }
+      /**
+       * Returns a string that describes the rectangle in the format:
+       * "Length: <length> Width: <width> Area: <area>".
+       * 
+       * @return a string describing the rectangle
+       */
+      public String toString() {
+    return "Length: " + length + "\n" + 
+           "Width: " + width + "\n" + 
+           "Area: " + findArea();
+}
+       
+      
+
+}
---
+++ RectangleDemo.java
@@ -0,0 +1,8 @@
+public class RectangleDemo{
+    public static void main(String[] args){
+        Rectangle r = new Rectangle(4,5);
+        System.out.println(r.toString());
+        System.out.println("Area: "+r.findArea());
+
+           }
+}
