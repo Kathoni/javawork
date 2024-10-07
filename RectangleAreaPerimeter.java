d2f4cc4f57cc624dc33d82cd436b56ffa73a1453
Kathoni <thrusherkeys@gmail.com>
2024-10-07 18:21:33 +0300

    completion of the RectangleAreaPerimeter

--- RectangleAreaPerimeter.java
+++ RectangleAreaPerimeter.java
@@ -7,6 +7,14 @@ public class RectangleAreaPerimeter{
         System.out.println("Enter your length : " );
         String name= RectanglePerimeter.nextLine();
         System.out.println("Enter your width : " );
+        String name2= RectanglePerimeter.nextLine();
+        length = Double.parseDouble(name);
+        width = Double.parseDouble(name2);
+        double area = length * width;
+        double perimeter = 2 * (length + width);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+
 
         
     }
