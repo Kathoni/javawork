f3d1035550770d41eacffd1547ce4440c91004a0
Kathoni <thrusherkeys@gmail.com>
2024-10-12 15:07:22 +0300

    ArrayList importation and Cruisehip class making

---
+++ Cruiseship.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Cruiseship{
+
+    ArrayList<Passenger> passengers;//You must declare the passengers first
+
+    public Cruiseship(){
+          passengers = new ArrayList<>();//innitializing the ArrayList
+    }
+    /*This method adds the inputed passenger p
+    * to the ArrayList passengers
+    */
+   public void addPassanger(Passenger P) {
+       passengers.add(P);
+       //Add passenger object to the list 
+   }
+   /*This method returns the number of 
+   * passengers in the list.
+   */
+   public int getNumPassangers(){
+       return passengers.size();
+   }
+   //Returns the size of the ArrayList
+    
+}
