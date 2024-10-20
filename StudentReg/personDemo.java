b9109c13320517393579ffef199ed9bf6f2721c5
Kathoni <thrusherkeys@gmail.com>
2024-10-20 11:55:09 +0300

    creating personDemo

---
+++ StudentReg/personDemo.java
@@ -0,0 +1,9 @@
+public class personDemo{
+
+    public static void main(String[] args) {
+
+        Person person = new Person("Sylvia", "Kathoni");
+        System.out.println(person.toString());
+
+    }
+}
